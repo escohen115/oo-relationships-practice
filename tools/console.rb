3af5bfa881fb696fc7233d34e824e76096b997ba
escohen115 <escohen115@gmail.com>
2020-11-01 20:41:43 -0500

    commit tree 89046654cb6733504c879549bc1ce97a03dc39ed

--- tools/console.rb
+++ tools/console.rb
@@ -4,5 +4,42 @@ def reload
   load 'config/environment.rb'
 end
 
-binding.pry
-0
+
+#Bakery ==> Deserts <==> Ingredients
+
+def Bakery
+  @@all = []
+  attr_accessor :desert
+  def initialize (name)
+    @name = name 
+    @@all << self
+  end
+
+  def ingredients
+    Dessert.all.map{|dessert|dessert.ingredient}
+  end
+
+  def desserts
+    Dessert.all.select{|dessert|dessert.bakery == self}
+  end
+
+  def average_calories
+    total = 0
+    arr = self.desserts.all.map{|dessert|dessert.ingredient.calories}
+    arr.each do |cal|
+      total += cal
+    end
+    total / arr.count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def shopping_list
+    self.desserts.map{|dessert|dessert.ingredient}
+  end
+
+
+end
+
